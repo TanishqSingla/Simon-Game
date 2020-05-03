02b1dc1c593e3b0e116d2227ad357d964bce3811
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-04 01:36:12 +0530

    Added level system

--- game.js
+++ game.js
@@ -6,6 +6,22 @@ var gamePattern = [];
 //Array for pattern user chose
 var userClickedPattern = [];
 
+//Check for game started or not
+var started = false;
+
+//level counter
+var level = 0;
+
+//Main
+$(document).keypress(function () {
+  if (!started) {
+    //changing text to level number
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 //this function beeps random color, and stores the color in gamePattern
 function nextSequence() {
   var randomNum = Math.floor(Math.random() * 4);
@@ -16,6 +32,10 @@ function nextSequence() {
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColor);
+
+  //Changing level number
+  level++;
+  $("#level-title").text("Level " + level);
 }
 
 //playSound function plays sound when user clicks colour and when computer gives pattern
