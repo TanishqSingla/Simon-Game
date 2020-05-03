95d4b7e9e4fc5d45fb7b6d76bbda2cce2b94b0a7
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-04 01:11:31 +0530

    Adding comments to code

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
+
+//Array for pattern given to user by computer
 var gamePattern = [];
 
+//Array for pattern user chose
 var userClickedPattern = [];
 
+//this function beeps random color, and stores the color in gamePattern
 function nextSequence() {
   var randomNum = Math.floor(Math.random() * 4);
 
@@ -11,15 +15,20 @@ function nextSequence() {
 
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+  playSound(randomChosenColor);
+}
+
+//playSound function plays sound when user clicks colour and when computer gives pattern
+function playSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
 }
 
+//Handler when user clicks a color
 $(`.btn`).on("click", function () {
   var userChosenColor = $(this).attr("id");
 
   userClickedPattern.push(userChosenColor);
 
-  var _audio = new Audio(`sounds/${userChosenColor}.mp3`);
-  _audio.play();
+  playSound(userChosenColor);
 });
